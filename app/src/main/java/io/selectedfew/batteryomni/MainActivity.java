cc1f9b81bd7eb19e98b2fcf6e97f6e0ee003b389
internot <internotx@gmail.com>
2025-06-21 14:06:15 -0400

    removed unused import

--- app/src/main/java/io/selectedfew/batteryomni/MainActivity.java
+++ app/src/main/java/io/selectedfew/batteryomni/MainActivity.java
@@ -8,7 +8,6 @@ import android.content.IntentFilter;
 import android.os.BatteryManager;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.ScrollView;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.File;
